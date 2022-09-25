8a419634f7e44d94425d5d91531670ac6f6a650d
DebajyotiGoswami <goswamidiary@gmail.com>
2022-09-25 13:15:42 +0530

    in process_billing() : mru finding line is now outside if statement

--- daily_billing_kalyani.py
+++ daily_billing_kalyani.py
@@ -8,6 +8,7 @@ def find_cell(sheet , max_row , max_col , heading):
             
 def process_files(files):
     for file in files:
+        #result = {"MPR" : [0 , 0]  , "QPR" : [0 , 0]}
         result = {"A" : [0 , 0] , "I" : [0 , 0] , "OTH_MONTHLY" : [0 , 0]  , "QPR" : [0 , 0]}
         sheet = xl.load_workbook(file).active
         max_row , max_col = sheet.max_row , sheet.max_column
@@ -16,8 +17,8 @@ def process_files(files):
         inv_row , inv_col = find_cell(sheet , max_row , max_col , "Invoice Generated")
 
         for i in range(1 , max_row + 1):
-            if sheet.cell(row = i , column = mru_col).value is not None and sheet.cell(row = i , column = mru_col).value.strip()[-3:] in ('QPR' , 'MPR'):
-                mru = sheet.cell(row = i , column = mru_col).value.strip()
+            mru = sheet.cell(row = i , column = mru_col).value
+            if mru is not None and mru.strip()[-3:] in ('QPR' , 'MPR'):
                 con = sheet.cell(row = i , column = con_col).value
                 inv = sheet.cell(row = i , column = inv_col).value
                 if con is None : con = 0   #where con count value is blank , consider it as ZERO
