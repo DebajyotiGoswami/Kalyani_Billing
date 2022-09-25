2cc75ada83b50a27cb2b2f54a261eb87bdedfe2d
DebajyotiGoswami <goswamidiary@gmail.com>
2022-09-25 11:52:40 +0530

    all filenames included in a list (files). Then the files list sent to process_files()

--- daily_billing_kalyani.py
+++ daily_billing_kalyani.py
@@ -1,7 +1,14 @@
 
 
+def process_files(files):
+    for file in files:
+        print(file)
+        
 def main():
     print("Hello . Rename CCC wise excels as 103.xlsx , 201.xlsx etc.")
-
+    files = ['103.xlsx' , '201.xlsx' , '202.xlsx' , '208.xlsx' , \
+             '300.xlsx' , '301.xlsx' , '401.xlsx' , '402.xlsx']
+    process_files(files)
+    
 if __name__ == '__main__':
     main()
