706ecc8cea5a3b923d6ccfaf379b3a48485995ed
DebajyotiGoswami <goswamidiary@gmail.com>
2022-09-25 13:00:38 +0530

    mru_wise_class excel files created and in main() : two different file name created

--- daily_billing_kalyani.py
+++ daily_billing_kalyani.py
@@ -29,9 +29,10 @@ def process_files(files):
         
 def main():
     print("Hello . Rename CCC wise excels as 103.xlsx , 201.xlsx etc.")
-    files = ['103.xlsx' , '201.xlsx' , '202.xlsx' , '208.xlsx' , \
+    bill_files = ['103.xlsx' , '201.xlsx' , '202.xlsx' , '208.xlsx' , \
              '300.xlsx' , '301.xlsx' , '401.xlsx' , '402.xlsx']
-    process_files(files)
+    mru_files = 'mru_wise_class.xlsx'
+    process_files(bill_files)
     
 if __name__ == '__main__':
     main()
